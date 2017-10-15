7837f98015ec254e8d1bd5b7efea2230508d8567
seanpierce <sumler.sean@gmail.com>
2017-10-15 07:33:23 -0700

    refactor redundant var declarations

--- Program.cs
+++ Program.cs
@@ -6,62 +6,55 @@ namespace SeanPierce.FitnessFrog
   {
     static void Main()
     {
-      double runningTotal = 0;
-      bool keepGoing = true;
+      var runningTotal = 0.0;
 
-      while (keepGoing)
+      while (true)
       {
         // Prompt user for minutes exercised
         Console.Write("Enter how many minutes you exercised, or type \"quit\" to exit: ");
 
-        string entry = Console.ReadLine();
+        var entry = Console.ReadLine();
 
         if (entry.ToLower() == "quit")
         {
-          keepGoing = false;
+          break;
+        }
+        try
+        {
+        var minutes = double.Parse(entry);
+
+        if (minutes <= 0)
+        {
+          Console.WriteLine(minutes + " is not an accepable value.");
+          continue;
+        }
+        else if (minutes <= 10)
+        {
+          Console.WriteLine("Better than nothing, right?");
+        }
+        else if (minutes <= 30)
+        {
+          Console.WriteLine("Way to go!");
+        }
+        else if (minutes <= 60)
+        {
+          Console.WriteLine("You're doing great!");
         }
         else
         {
-
-          try
-          {
-            double minutes = double.Parse(entry);
-
-            if (minutes <= 0)
-            {
-              Console.WriteLine(minutes + " is not an accepable value.");
-              continue;
-            }
-            else if (minutes <= 10)
-            {
-              Console.WriteLine("Better than nothing, right?");
-            }
-            else if (minutes <= 30)
-            {
-              Console.WriteLine("Way to go!");
-            }
-            else if (minutes <= 60)
-            {
-              Console.WriteLine("You're doing great!");
-            }
-            else
-            {
-              Console.WriteLine("Holy Cow! Slow down!");
-            }
-            // Add minutes exercised to total
-            runningTotal = runningTotal + minutes;
-          }
-          catch (FormatException)
-          {
-            Console.WriteLine("That is not valid input.");
-            continue;
-          }
-
-          // Display total minutes exercised to the screen
-          Console.WriteLine("You've exercised for " + runningTotal + " minutes.");
-          // Repeat until user quits
+          Console.WriteLine("Holy Cow! Slow down!");
         }
-
+        // Add minutes exercised to total
+        runningTotal += minutes;
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("That is not valid input.");
+        continue;
+      }
+      // Display total minutes exercised to the screen
+      Console.WriteLine("You've exercised for " + runningTotal + " minutes.");
+      // Repeat until user quits
       }
       Console.Write("Goodbye!");
     }
